b18172696e7dbc53256321a98bfa549865ff5199
KyleNo <kylenoble8@gmail.com>
2015-12-08 09:49:35 -0800

    yolo

--- app/src/main/java/com/example/admin/untouchable/MainActivity.java
+++ app/src/main/java/com/example/admin/untouchable/MainActivity.java
@@ -1,24 +1,56 @@
 package com.example.admin.untouchable;
 
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.UserHandle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     boolean firstDraw = true;
     int cardNumber=7;
+    int cardsOnScreen=7;
+    int[] cardValues;
 
-
+    //Context context = this.getApplicationContext();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,8 +58,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         LinearLayout yep = (LinearLayout) findViewById(R.id.okay);
         //ImageButton[] cards = new ImageButton[7];
-        String names ="card";
-
         String Convert;
 
         for(int i=0; i<7; i++){
@@ -42,17 +72,43 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if(firstDraw){
+
                     for(int i=0; i<7; i++){
                         showCard(i);
                     }
                     firstDraw=false;
+
                 }
                 else{
                     cardNumber++;
+                    createCard();
+                    for(int i=0; i<cardNumber; i++){
+                        //resize image buttons???
+                        Log.v("what","?");
+                        resizeCards(i);
+                    }
+
                 }
             }
         });
     }
+
+    protected void resizeCards(int i){
+        LinearLayout yep = (LinearLayout) findViewById(R.id.okay);
+        ImageButton card = (ImageButton) findViewById(i);
+        //?????
+    }
+
+    protected void createCard(){
+        ImageButton card = new ImageButton (this.getApplicationContext());
+        card.setId(cardNumber + 0);
+        LinearLayout yep = (LinearLayout) findViewById(R.id.okay);
+        yep.addView(card);
+        hideCard(cardNumber);
+        pickCard(cardNumber);
+        showCard(cardNumber);
+    }
+
 
     protected void hideCard(int i){
         ImageButton cards = (ImageButton) findViewById(i);
@@ -70,7 +126,9 @@ public class MainActivity extends AppCompatActivity {
                 R.drawable.yellow1,R.drawable.yellow2,R.drawable.yellow3,R.drawable.yellow4,
                 R.drawable.blue1,R.drawable.blue2,R.drawable.blue3,R.drawable.blue4,
                 R.drawable.green1,R.drawable.green2,R.drawable.green3,R.drawable.green4};
-        card.setImageResource(names[rng.nextInt(16)]);
+        cardValues[i]=rng.nextInt(16);
+        int temp=cardValues[i];
+        card.setImageResource(names[temp]);
     }
 
     protected void showCard(int i){
